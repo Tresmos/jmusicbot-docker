d38580078180d59359810196ecaec132ba313db7
Tresmos <root@tresmos.wtf>
2024-04-24 04:07:54 +0300

    Remove color and improve shutdown time

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,21 +1,18 @@
 #!/bin/sh
-BCYAN='\033[1;36m'
-BGREEN='\033[1;32m' 
-NC='\033[0m'
 
 EXAMPLE_CONFIG=https://github.com/Tresmos/jmusicbot-docker/raw/main/config.txt
 CONFIG_FILE=/opt/jmusicbot/data/config.txt
 
 set -e
 
-echo "JMusicBot Version: ${BCYAN}$BOT_VERSION${NC}"
+echo "JMusicBot Version: $BOT_VERSION"
 
 if [ ! -f "$CONFIG_FILE" ]; then
-    echo "${BCYAN}config.txt${NC} not found. Downloading example config file..."
+    echo "config.txt not found. Downloading example config file..."
     curl -L -sS -o ${CONFIG_FILE} ${EXAMPLE_CONFIG}
-    echo "${BGREEN}Example config file downloaded. Please edit the config file and restart the container. "
+    echo "Example config file downloaded. Please edit the config file and restart the container. "
     exit
 fi
 
 cd /opt/jmusicbot/data/
-java -jar -Dconfig=/opt/jmusicbot/data/config.txt -Dnogui=true /opt/jmusicbot/JMusicBot.jar
+exec java -jar -Dconfig=/opt/jmusicbot/data/config.txt -Dnogui=true /opt/jmusicbot/JMusicBot.jar
